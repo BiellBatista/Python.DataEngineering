9e2fabae851dcbb26337bad28d23c65292d17ecf
Gabriel Batista <gabriel.texteis@gmail.com>
2026-09-18 00:38:14 -0300

    conclusão da aula 03 do curso "Imersão Engenharia de Dados com IA da Alura"

---
+++ Alura-Imersao-Engenharia-Dados-IA/sql/00-preparar-ambiente.sql
@@ -0,0 +1,6 @@
+-- Execute em ambiente de estudos com permissão para criar estes objetos.
+CREATE CATALOG IF NOT EXISTS voebem;
+CREATE SCHEMA IF NOT EXISTS voebem.bronze;
+CREATE SCHEMA IF NOT EXISTS voebem.silver;
+CREATE SCHEMA IF NOT EXISTS voebem.gold;
+CREATE VOLUME IF NOT EXISTS voebem.bronze.arquivos;
